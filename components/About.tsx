01841e9b1c6d749cc346f12b0289dfa969d40d09
AndreyDiak <brodiaga_0880@mail.ru>
2022-09-21 18:24:08 +0300

    portfolio v1

---
+++ components/About.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import { customColor } from '../pages'
+type Props = {}
+
+export default function About({ }: Props) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 3.5 }}
+      className='flex flex-col relative h-screen text-center md:text-left md:flex-row 
+      max-w-7xl px-10 justify-evenly mx-auto items-center'
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+      <motion.img
+        initial={{
+          x: -200,
+          opacity: 0
+        }}
+        transition={{
+          duration: 1.2
+        }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: false }}
+        src="/about.png"
+        alt=""
+        className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
+        md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[700px]'
+      />
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{' '}
+          <span className={`underline decoration-[#f7ab0a]/50`}>little</span>{' '}
+          background
+        </h4>
+        <p className='text-base text-gray-200'>
+          Привет! Меня зовут Андрей и я <span className='font-bold'>ВЕБ-РАЗРБОТЧИК!</span> 
+          Я учусь на 4 курсе в РТУ МИРЭА.
+          Я начал увлекаться компьютерными технологиями с 8 класса и с тех пор решил связать свою жизнь 
+          с <b>IT</b>.
+          В 11 классе я начал заниматься версткой простых сайтов и с тех пор имею за плечами не мало готовых 
+          макетов. Уже в университете я познакомился с <b>JavaScript</b>-фреймворками и решил выбрать <b>React</b>.
+          На изучение самого React-а у меня ушло около полугода после чего я стал делать свои пет-проекты
+          с использованием различных библиотек а так-же хранилищ данных, таких как MongoDB, SanityIo, FireBase
+          и SupaBase. Я не берусь утверждать, что обладаю всеми необходыми мне знаниями, но у меня есть желанию
+          развиваться и продолжать учится, а это, как по мне, самое главное!
+        </p>
+      </div>
+    </motion.div>
+  )
+}
---
+++ components/Hero.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import { customColor } from '../pages'
+import { BackCircles } from './BackCircles'
+
+type Props = {}
+
+export default function Hero({ }: Props) {
+
+  const [text] = useTypewriter({
+    words: [
+      'Nice/to/meet/You.html',
+      'My_Name_Is_Andrey.css',
+      'Guy-who-loves-Tea.tsx',
+      '<ButLovesToCodeMore />'
+    ],
+    loop: true,
+    delaySpeed: 2000
+  })
+
+  return (
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+      <BackCircles />
+      <img
+        className='relative rounded-full h-32 w-32 object-cover'
+        src="/me.jpg"
+        alt=""
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Softwate Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className='mr-3 font-mono'>{text}</span>
+          <Cursor cursorColor={customColor} />
+        </h1>
+
+        <div className="pt-2">
+          <Link href='#about'><button className="heroButton">About</button></Link>
+          <Link href='#experience'><button className="heroButton">Experience</button></Link>
+          <Link href='#skills'><button className="heroButton">Skills</button></Link>
+          <Link href='#projects'><button className="heroButton">Projects</button></Link>
+        </div>
+
+      </div>
+    </div>
+  )
+}
